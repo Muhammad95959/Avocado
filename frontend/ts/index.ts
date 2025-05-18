9f9fb1f61f1a3d4bbfc866374506091155791457
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2025-05-18 21:18:49 +0300

    add top dishes section desgin

--- frontend/ts/index.ts
+++ frontend/ts/index.ts
@@ -1,9 +1,10 @@
-import { menu_list } from "./utils/assets";
+import { menu_list, food_list } from "./utils/assets";
 
 const navbarMenuItems = Array.from(
   document.querySelectorAll(".navbar-menu li") as NodeListOf<HTMLLIElement>,
 );
 const menuChoices = document.querySelector(".menu-choices") as HTMLDivElement;
+const topDishesCardsContainer = document.querySelector(".top-dishes .cards") as HTMLDivElement;
 
 // handle navbar menu items click event
 navbarMenuItems.forEach((li) => {
@@ -37,4 +38,85 @@ menu_list.forEach((val) => {
   name.classList.add("name");
   name.textContent = val.menu_name;
   menuChoice.appendChild(name);
+});
+
+// dynamically add top dishes
+food_list.forEach((val) => {
+  const card = document.createElement("div");
+  card.classList.add("card");
+  card.dataset.cartItemsCount = String(0);
+  topDishesCardsContainer.appendChild(card);
+  const imgContainer = document.createElement("div");
+  imgContainer.classList.add("img-container");
+  card.appendChild(imgContainer);
+  const img = document.createElement("img");
+  img.classList.add("dish-photo");
+  img.src = val.image;
+  imgContainer.appendChild(img);
+  const addIcon = document.createElement("img");
+  addIcon.classList.add("add");
+  addIcon.src = "../assets/icon_add_white.png";
+  addIcon.tabIndex = 0;
+  if (card.dataset.cartItemsCount !== "0") addIcon.classList.add("hidden");
+  imgContainer.appendChild(addIcon);
+  const cartHandler = document.createElement("div");
+  cartHandler.classList.add("cart-handler");
+  if (card.dataset.cartItemsCount === "0") cartHandler.classList.add("hidden");
+  imgContainer.appendChild(cartHandler);
+  const decIcon = document.createElement("img");
+  decIcon.classList.add("dec");
+  decIcon.src = "../assets/icon_remove_red.png";
+  decIcon.tabIndex = 0;
+  cartHandler.appendChild(decIcon);
+  const cartItemsCount = document.createElement("p");
+  cartItemsCount.classList.add("cart-items-count");
+  cartItemsCount.textContent = card.dataset.cartItemsCount;
+  cartHandler.appendChild(cartItemsCount);
+  const incIcon = document.createElement("img");
+  incIcon.classList.add("inc");
+  incIcon.src = "../assets/icon_add_green.png";
+  incIcon.tabIndex = 0;
+  cartHandler.appendChild(incIcon);
+  const info = document.createElement("div");
+  info.classList.add("info");
+  card.appendChild(info);
+  const row = document.createElement("div");
+  row.classList.add("row");
+  info.appendChild(row);
+  const name = document.createElement("h3");
+  name.classList.add("name");
+  name.textContent = val.name;
+  row.appendChild(name);
+  const ratingImg = document.createElement("img");
+  ratingImg.src = "../assets/rating_starts.png";
+  row.appendChild(ratingImg);
+  const desc = document.createElement("p");
+  desc.classList.add("description");
+  desc.textContent = val.description;
+  info.appendChild(desc);
+  const price = document.createElement("p");
+  price.classList.add("price");
+  price.textContent = `$${val.price}`;
+  info.appendChild(price);
+
+  addIcon.addEventListener("click", () => {
+    card.dataset.cartItemsCount = String(1);
+    addIcon.classList.add("hidden");
+    cartHandler.classList.remove("hidden");
+    cartItemsCount.textContent = String(1);
+  });
+  decIcon.addEventListener("click", () => {
+    if (card.dataset.cartItemsCount === String(1)) {
+      addIcon.classList.remove("hidden");
+      cartHandler.classList.add("hidden");
+      cartItemsCount.textContent = String(0);
+    } else {
+      card.dataset.cartItemsCount = String(parseInt(card.dataset.cartItemsCount as string) - 1);
+      cartItemsCount.textContent = card.dataset.cartItemsCount;
+    }
+  });
+  incIcon.addEventListener("click", () => {
+    card.dataset.cartItemsCount = String(parseInt(card.dataset.cartItemsCount as string) + 1);
+    cartItemsCount.textContent = card.dataset.cartItemsCount;
+  });
 });
